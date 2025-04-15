b080a5da92547819bf588587952dcc3530cae129
Charishma Pothireddy <cherryspl09@gmail.com>
2025-04-15 10:54:23 +0530

    Create RedheadDuck.java

---
+++ Strategy SimUDuck interface design/RedheadDuck.java
@@ -0,0 +1,11 @@
+public class RedheadDuck extends Duck{
+    public void quack(){
+        System.out.println("Quacking...");
+    }
+    public void fly(){
+        System.out.println("Flying...");
+    }
+    public void display(){
+        System.out.println("Redhead Duck Displaying...");
+    }
+}
