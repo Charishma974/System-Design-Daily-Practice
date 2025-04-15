4c8f98fb26f875e4d706c2cc822bb155f18e25bc
Charishma Pothireddy <cherryspl09@gmail.com>
2025-04-15 10:21:04 +0530

    Create Duck.java

---
+++ Strategy Simple Duck/Duck.java
@@ -0,0 +1,9 @@
+public abstract class Duck{
+    public void quack(){
+        System.out.println("Quacking...");
+    }
+    public void swimming(){
+        System.out.println("Swimming...");
+    }
+    public abstract void display();
+}
