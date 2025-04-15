5525270fc3bd2703157aa20736c9e8814602b0ff
Charishma Pothireddy <cherryspl09@gmail.com>
2025-04-15 10:55:13 +0530

    Create RubberDuck.java

---
+++ Strategy SimUDuck interface design/RubberDuck.java
@@ -0,0 +1,8 @@
+public class RubberDuck extends Duck implements Quackable{
+    public void quack(){
+        System.out.println("Quacking...");
+    }
+    public void display(){
+        System.out.println("Rubber Duck Displaying...");
+    }
+}
